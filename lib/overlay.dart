c1c15aea80a43d323fe6ed9a42c1e1441f7caa26
XinyueZ <dev.xinyue.zhao@gmail.com>
2020-05-26 22:14:47 +0200

    Update Overlay with toast

--- lib/overlay.dart
+++ lib/overlay.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_face_mask_detecting/toast/toast.dart';
 
 class Overlay extends StatelessWidget {
   const Overlay({
     @required List<dynamic> results,
+    this.threshold = 0.8,
   }) : _results = results;
   final List<dynamic> _results;
+  final double threshold;
 
-  Color _updateBorderColor(List<dynamic> bits) {
+  Color _updateBorderColor(
+    BuildContext context,
+    List<dynamic> bits,
+  ) {
     if (bits == null) {
       return Colors.transparent;
     }
 
     String label;
+    double confidence = 0;
+
     if (bits.length > 1) {
       final String firstLabel = bits.first["label"] as String;
       final double firstConfidence = bits.first["confidence"] as double;
@@ -21,12 +29,24 @@ class Overlay extends StatelessWidget {
 
       if (firstConfidence > secondConfidence) {
         label = firstLabel;
+        confidence = firstConfidence;
       } else {
         label = secondLabel;
+        confidence = secondConfidence;
       }
     }
     if (bits.length == 1) {
       label = bits.first["label"] as String;
+      confidence = bits.first["confidence"] as double;
+    }
+
+    if (confidence < threshold) {
+      Toast.show(
+        context,
+        ToastType.error,
+        "Please keep the camera steady and keep a certain distance to prevent shaking.",
+      );
+      return Colors.transparent;
     }
 
     if (label == "without_mask") {
@@ -36,6 +56,7 @@ class Overlay extends StatelessWidget {
     if (label == "with_mask") {
       return Colors.greenAccent;
     }
+
     return Colors.transparent;
   }
 
@@ -44,7 +65,10 @@ class Overlay extends StatelessWidget {
     return Container(
       decoration: BoxDecoration(
         border: Border.fromBorderSide(BorderSide(
-          color: _updateBorderColor(_results),
+          color: _updateBorderColor(
+            context,
+            _results,
+          ),
           width: 15,
         )),
       ),
