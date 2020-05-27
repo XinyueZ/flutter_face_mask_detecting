26eba893c75e0b22ff80e11c4aed9095dfdfdaaa
XinyueZ <dev.xinyue.zhao@gmail.com>
2020-05-27 22:26:49 +0200

    Refine overlay

--- lib/overlay.dart
+++ lib/overlay.dart
@@ -1,13 +1,37 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_face_mask_detecting/toast/toast.dart';
+import 'package:oktoast/oktoast.dart';
 
-class Overlay extends StatelessWidget {
+class Overlay extends StatefulWidget {
   const Overlay({
     @required List<dynamic> results,
-    this.threshold = 0.8,
+    this.threshold = 0.5,
   }) : _results = results;
   final List<dynamic> _results;
   final double threshold;
+
+  @override
+  _OverlayState createState() => _OverlayState();
+}
+
+class _OverlayState extends State<Overlay> {
+  ToastFuture _toastFuture;
+  String _label;
+  double _confidence = 0;
+
+  set label(String value) => setState(() {
+        _label = value;
+      });
+
+  set confidence(double value) => setState(() {
+        _confidence = value;
+      });
+
+  String get label => _label;
+
+  double get confidence => _confidence;
 
   Color _updateBorderColor(
     BuildContext context,
@@ -16,9 +40,6 @@ class Overlay extends StatelessWidget {
     if (bits == null) {
       return Colors.transparent;
     }
-
-    String label;
-    double confidence = 0;
 
     if (bits.length > 1) {
       final String firstLabel = bits.first["label"] as String;
@@ -40,12 +61,15 @@ class Overlay extends StatelessWidget {
       confidence = bits.first["confidence"] as double;
     }
 
-    if (confidence < threshold) {
-      Toast.show(
-        context,
-        ToastType.error,
-        "Please keep the camera steady and keep a certain distance to prevent shaking.",
-      );
+    if (confidence < widget.threshold) {
+      scheduleMicrotask(() => _toastFuture ??= Toast.show(
+              context,
+              ToastType.error,
+              "Please keep the camera steady and keep a certain distance to prevent shaking.",
+              onDismiss: () {
+            _toastFuture = null;
+          }));
+
       return Colors.transparent;
     }
 
@@ -62,16 +86,54 @@ class Overlay extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        border: Border.fromBorderSide(BorderSide(
-          color: _updateBorderColor(
-            context,
-            _results,
+    return Stack(
+      children: <Widget>[
+        Container(
+          decoration: BoxDecoration(
+            border: Border.fromBorderSide(BorderSide(
+              color: _updateBorderColor(
+                context,
+                widget._results,
+              ),
+              width: 10,
+            )),
           ),
-          width: 15,
-        )),
-      ),
+        ),
+        Container(
+          margin: const EdgeInsets.symmetric(
+            horizontal: 85,
+          ),
+          child: Column(
+            children: <Widget>[
+              SizedBox(
+                height: MediaQuery.of(context).padding.top,
+              ),
+              Align(
+                alignment: Alignment.center,
+                child: Text(
+                  label == "with_mask"
+                      ? "Wearing mask ${(confidence * 100).toStringAsFixed(0)}%"
+                      : "No mask ${(confidence * 100).toStringAsFixed(0)}%",
+                  style: Theme.of(context).textTheme.caption.copyWith(
+                        color: label == "with_mask"
+                            ? Colors.greenAccent
+                            : Colors.red,
+                      ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.center,
+                child: LinearProgressIndicator(
+                  value: confidence,
+                  valueColor: AlwaysStoppedAnimation<Color>(
+                      label == "with_mask" ? Colors.greenAccent : Colors.red),
+                  minHeight: 15,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
